2240d7a4b868cb594b7d83544e1b6db4df806e5e
Jarryd Tilbrook <jarryd@tilbrooktech.com>
2022-04-06 18:31:09 +0800

    Add support for BDD style tests (#644)
    This is provided by the plugin https://github.com/milroyfraser/pest-plugin-gwt

--- spec/fixtures/pest/PestTest.php
+++ spec/fixtures/pest/PestTest.php
@@ -7,3 +7,7 @@ it('is a test case', function () {
 test('with a different descriptor', function () {
     //
 });
+
+scenario('bdd flavour')
+    ->when(fn () => true)
+    ->then(fn ($result) => true);

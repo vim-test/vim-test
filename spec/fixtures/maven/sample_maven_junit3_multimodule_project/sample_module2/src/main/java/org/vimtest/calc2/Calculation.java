ddc2854a3d4c0fccbdd61af7d80fe2e3bcc80a97
Peppe <giuseppemath@gmail.com>
2023-07-05 16:11:06 -0300

    chore(fixture): add another module to sample_maven_multimodule (#677)

---
+++ spec/fixtures/maven/sample_maven_junit3_multimodule_project/sample_module2/src/main/java/org/vimtest/calc2/Calculation.java
@@ -0,0 +1,11 @@
+package org.vimtest.calc2;
+
+public class Calculation {
+    static public int add(int a, int b) {
+        return a + b;
+    }
+
+    static public int sub(int a, int b) {
+        return a - b;
+    }
+}

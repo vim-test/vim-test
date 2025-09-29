68d3e52b0f4a1da02b19213d51940cb91883506a
Mathew Attlee <hello@codeinabox.com>
2025-09-29 14:48:50 +0100

    Improve Vitest detection using import (#867)

--- spec/fixtures/vitest/__tests__/context-test.jsx
+++ spec/fixtures/vitest/__tests__/context-test.jsx
@@ -1,3 +1,5 @@
+import { describe, context, it } from 'vitest';
+
 describe("Math", function () {
   context("Addition", function () {
     it("adds two numbers", function () {
--- spec/fixtures/vitest/__tests__/escaping-test.ts
+++ spec/fixtures/vitest/__tests__/escaping-test.ts
@@ -1,3 +1,5 @@
+import { describe, it } from 'vitest';
+
 describe('Escaping', () => {
   it('parentheses (', () => {
     // assertions
